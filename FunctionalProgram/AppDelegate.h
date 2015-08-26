38132a1bd5c9718e8b3471ab738034969222ae45
祝文博 <wenbo.zhu@tqmall.com>
2015-08-26 17:00:58 +0800

    Initial Commit

---
+++ FunctionalProgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunctionalProgram
+//
+//  Created by 祝文博 on 15/8/26.
+//  Copyright (c) 2015年 KevinLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunctionalProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunctionalProgram
+//
+//  Created by 祝文博 on 15/8/26.
+//  Copyright (c) 2015年 KevinLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
