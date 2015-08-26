d03ea348879593637a4782132b2d51922d7cb004
祝文博 <wenbo.zhu@tqmall.com>
2015-08-26 20:51:22 +0800

    Flow and RACSequence

--- FunctionalProgram/ViewController.m
+++ FunctionalProgram/ViewController.m
@@ -18,6 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSArray *array = @[@1, @2, @3];
+    RACSequence *stream = [array rac_sequence];
+    
+    NSLog(@"%@", [[stream map:^id(id value) {
+        return @(pow([value integerValue], 2));
+    }] array]);
+    
+    
+    NSLog(@"%@", [[stream filter:^BOOL(id value) {
+        return [value integerValue] % 2 == 0;
+    }] array]);
+    
+    NSLog(@"%@", [[stream map:^id(id value) {
+        return [value stringValue];
+    }] foldLeftWithStart:@"" reduce:^id(id accumulator, id value) {
+        return [accumulator stringByAppendingString:value];
+    }]);
 }
 
 - (void)didReceiveMemoryWarning {
